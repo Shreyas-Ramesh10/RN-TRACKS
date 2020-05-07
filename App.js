02d6d68550fbc8cdedddbf3a1770dfaf731b3ed6
Shreyas-Ramesh10 <59929353+Shreyas-Ramesh10@users.noreply.github.com>
2020-05-07 13:39:00 +0530

    Add files via upload
    App v2

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
@@ -8,38 +11,50 @@ import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
-import {Provider as AuthProvider} from './src/screens/context/AuthContext';
-import { setNavigator } from './src/screens/navigationRef';
-import ResolveAuthScreen from './src/ResolveAuthScreen';
-import { Provider as LocationProvider } from './src/screens/context/LocationContext'
+import { Provider as AuthProvider } from './src/context/AuthContext';
+import { setNavigator } from './src/navigationRef';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
+import { Provider as LocationProvider } from './src/context/LocationContext';
+import { Provider as TrackProvider } from './src/context/TrackContext';
+import { FontAwesome } from '@expo/vector-icons';
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Tracks',
+  tabBarIcon: <FontAwesome name="th-list" size={20} />
+};
 
 const switchNavigator = createSwitchNavigator({
-    ResolveAuth: ResolveAuthScreen,
-    loginFlow: createStackNavigator({
-        Signup:SignupScreen,
-        Signin: SigninScreen,
-        
-    }),
-    mainFlow: createBottomTabNavigator({
-        trackListFlow:createStackNavigator({
-            TrackList: TrackListScreen,
-            TrackDetail: TrackDetailScreen
-            
-        }),
-        TrackCreate: TrackCreateScreen,
-        Account: AccountScreen
-    })
+  ResolveAuth: ResolveAuthScreen,
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow,
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
 });
 
 const App = createAppContainer(switchNavigator);
 
 export default () => {
-    return(
-        <LocationProvider>
-    <AuthProvider>
-        <App ref = {(navigator) => {setNavigator(navigator) }}/>
-    </AuthProvider>
-    </LocationProvider>
-    );
-
+  return (
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App
+            ref={navigator => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
+  );
 };
